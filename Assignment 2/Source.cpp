1746c04596559d89f1a557183ab09980441a67af
RyanCoding19 <Ryan_Wallace2019@outlook.com>
2023-09-15 18:59:43 -0400

    Added vector space for first and last and parsed data into structure object

--- Assignment 2/Source.cpp
+++ Assignment 2/Source.cpp
@@ -1,4 +1,53 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+#define STUDENT_NUMBER 28
+using namespace std;
+
+
+struct STUDENT_DATA
+{
+	string firstName;
+	string lastName;
+};
+
+
+
+
 int main()
 {
-	return 1;
+	string line;
+	int counter = 0;
+	ifstream myfile;
+	STUDENT_DATA names[STUDENT_NUMBER];
+	myfile.open("StudentData.txt");
+
+
+
+	if (myfile.is_open())
+	{
+		while (getline(myfile, line))
+		{
+			size_t pos = line.find_first_of(',');
+			names[counter].lastName = line.substr(0, pos);
+			names[counter].firstName = line.substr(pos + 1);
+			counter++;
+		}
+		myfile.close();
+
+		vector<string> FirstData;
+		vector<string> LastData;
+
+		for (int i = 0; i < STUDENT_NUMBER; i++)
+		{
+			FirstData.push_back(names[i].firstName);
+			LastData.push_back(names[i].lastName);
+		}
+		
+	}
+	else cout << "Can't read file";
+
+	return 0;
 }
