1ddfe1c27023b85f9a08c5e9162806dac50da54c
RyanCoding19 <Ryan_Wallace2019@outlook.com>
2023-09-15 22:14:56 -0400

    finished pre release code and and polished debug

--- Assignment 2/Source.cpp
+++ Assignment 2/Source.cpp
@@ -12,6 +12,7 @@ struct STUDENT_DATA
 {
 	string firstName;
 	string lastName;
+	string email;
 };
 
 
@@ -21,14 +22,17 @@ int main()
 {
 	string line;
 	int counter = 0;
+	int counter2 = 0;
 	ifstream myfile;
 	STUDENT_DATA names[STUDENT_NUMBER];
-	myfile.open("StudentData.txt");
 
 	vector<string> FirstData;
 	vector<string> LastData;
 	vector<string> email;
 
+#ifdef _DEBUG
+	myfile.open("StudentData.txt");
+	cout << "application running standard" << endl;
 	if (myfile.is_open())
 	{
 		while (getline(myfile, line))
@@ -59,5 +63,49 @@ int main()
 		cout << "First Name:" << FirstData[i] << endl;
 		cout << "Last Name: " << LastData[i] << endl;
 	}
+
+
+#else
+	myfile.open("StudentData_Emails.txt");
+	cout << "application running pre-release" << endl;
+	if (myfile.is_open())
+	{
+		while (getline(myfile, line))
+		{
+			size_t pos = line.find_first_of(',');
+			size_t pos2 = line.find_last_of(',');
+			names[counter2].lastName = line.substr(0, pos);
+			names[counter2].firstName = line.substr(pos+1, pos2-pos-1);
+			names[counter2].email = line.substr(pos2 + 1);
+			counter2++;
+		}
+		myfile.close();
+
+		for (int i = 0; i < STUDENT_NUMBER; i++)
+		{
+			FirstData.push_back(names[i].firstName);
+			LastData.push_back(names[i].lastName);
+			email.push_back(names[i].email);
+		}
+
+
+	}
+	else cout << "Can't read file";
+
+
+
+
+	for (int i = 0; i < STUDENT_NUMBER; i++)
+	{
+		cout << "Student #" << i + 1 << endl;
+		cout << "First Name:" << FirstData[i] << endl;
+		cout << "Last Name: " << LastData[i] << endl;
+		cout << "email: " << email[i] << endl;
+	}
+#endif
+
+
+
+
 	return 0;
 }
